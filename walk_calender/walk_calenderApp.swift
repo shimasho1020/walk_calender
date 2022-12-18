45cc30df8f5aae20a424fddb7dbe367dbe0eca4f
shimasho1020 <shimasho1020@gmail.com>
2022-12-18 13:47:58 +0900

    Initial Commit

---
+++ walk_calender/walk_calenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  walk_calenderApp.swift
+//  walk_calender
+//
+//  Created by 島田将太郎 on 2022/12/18.
+//
+
+import SwiftUI
+
+@main
+struct walk_calenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
